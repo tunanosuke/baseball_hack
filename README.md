83fafcabe8b9d9f3bac66ce48d07dc4c351a90fe
Yamashita Manato <yamashita.manato@moneyforward.co.jp>
2017-06-23 12:30:19 +0900

    Output .py file

--- README.md
+++ README.md
@@ -16,13 +16,13 @@ In the case of macox, change matplotlibrc as follows,
 $ `vi $(python -c "import matplotlib;print(matplotlib.matplotlib_fname())")`
 ```
 
-from
+**before**
 
 ```
 backend      : macosx
 ```
 
-to
+**after**
 
 ```
 backend      : Tkagg
@@ -34,24 +34,48 @@ backend      : Tkagg
 $ jupyter notebook
 ```
 
-## Data
+## Tips
 
-### Pitch Type
+### After saving, output .py file
+
+Generate jupyter_notebook_config.py
 
 ```
-"CH": "Change-up",
-"CU": "Curveball",
-"EP": "Ephuus",
-"FA": "Fastball",
-"FC": "Cut Fastball",
-"FF": "four-seam Fastball",
-"FO": "Forkball",
-"FS": "Split-finger Fastball",
-"FT": "two-seam Fastball",
-"KC": "Knuckle Curve",
-"KN": "Knuckleball",
-"SC": "Screwball",
-"SI": "Sinker",
-"SL": "Slider",
-"UN": "Unknown"
+jupyter notebook --generate-config
+```
+
+**before**
+
+```
+#c.FileContentsManager.post_save_hook = None
+```
+
+**after**
+
+```
+import io
+import os
+from notebook.utils import to_api_path
+
+_script_exporter = None
+
+def script_post_save(model, os_path, contents_manager, **kwargs):
+    from nbconvert.exporters.script import ScriptExporter
+
+    if model["type"] != "notebook":
+        return
+
+    global _script_exporter
+    if _script_exporter is None:
+        _script_exporter = ScriptExporter(parent=contents_manager)
+    log = contents_manager.log
+
+    base, ext = os.path.splitext(os_path)
+    py_fname = base + ".py"
+    script, resources = _script_exporter.from_filename(os_path)
+    script_fname = base + resources.get("output_extension", ".txt")
+    log.info("Saving script /%s", to_api_path(script_fname, contents_manager.root_dir))
+    with io.open(script_fname, "w", encoding="utf-8") as f:
+        f.write(script)
+c.FileContentsManager.post_save_hook = script_post_save
 ```
---
+++ code/tanaka_shutout_vs_redsox.py
@@ -0,0 +1,136 @@
+
+# coding: utf-8
+
+# In[1]:
+
+
+get_ipython().magic('matplotlib inline')
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as pd
+from collections import OrderedDict
+
+import time
+
+
+# In[2]:
+
+
+data = pd.read_csv("../data/mlbam_pitch_20170427.csv").query("pit_box_name=='Tanaka'")
+
+
+# In[3]:
+
+
+data[["inning_number", "pit_box_name", "bat_box_name", "bat_hand_cd", "px", "pz", "sz_top", "sz_bot", "pitch_type", "pitch_res", "start_speed", "pa_ball_ct", "pa_strike_ct", "pitch_des"]]
+
+
+# In[4]:
+
+
+sns.palplot(sns.hls_palette(24, l = 0.5, s = 1))
+
+
+# In[8]:
+
+
+PITCH_TYPES = {
+    "CH": "Change-up",
+    "CU": "Curve",
+    "EP": "Ephuus",
+    "FA": "Fastball",
+    "FC": "Cut",
+    "FF": "Four-seam",
+    "FO": "Fork",
+    "FS": "Split",
+    "FT": "Two-seam",
+    "KC": "Knuckle Curve",
+    "KN": "Knuckle",
+    "SC": "Screw",
+    "SI": "Sinker",
+    "SL": "Slider",
+    "UN": "Unknown"
+}
+
+
+# In[9]:
+
+
+def get_marker(type):
+    if type in ("FA", "FC", "FF", "FT"):
+        return "o"
+    elif type in ("CH", "FO", "FS", "KN", "SC"):
+        return "v"
+    elif type in ("CU", "KN", "SL"):
+        return "<"
+    elif type == "SI":
+        return ">"
+    return "H"
+
+
+# In[10]:
+
+
+inning = 1
+batter = "Pedroia"
+pitch = 1
+cnt = 1
+
+
+# In[11]:
+
+
+sns.set_context("notebook", 1.5, {"lines.linewidth": 2})
+
+fig = plt.figure()
+ax = fig.add_subplot(1, 1, 1)
+ax.set_title("Inning {inning} Ma-kun vs {batter}".format(inning = inning, batter = batter))
+ax.set_xlabel("px")
+ax.set_xlim((3.0, -3.0))
+ax.set_ylabel("pz")
+ax.set_ylim((0.0, 5.0))
+ax.grid(True)
+
+# Strike Zone
+ax.vlines(-0.8, ymin =1.5, ymax = 3.32)
+ax.vlines(0.8, ymin =1.5, ymax = 3.32)
+ax.hlines(1.5, xmin = -0.8, xmax = 0.8)
+ax.hlines(3.32, xmin = -0.8, xmax = 0.8)
+
+pitch_data = []
+for _, d in data.iterrows():
+    if inning == d["inning_number"] and batter == d["bat_box_name"]:
+        pitch_data.append(d)
+
+for i in range(0, pitch):
+    label = "{cnt}:{event} ({type}, {speed}km/h)".format(
+        **{
+            "cnt": i + 1,
+            "event": pitch_data[i]["pitch_des"],
+            "type": PITCH_TYPES.get(pitch_data[i]["pitch_type"]),
+            "speed": round(pitch_data[i]["start_speed"] * 1.6093),
+        }
+    )
+    color = sns.hls_palette(24, l = 0.5, s = 1)[i]
+    marker = get_marker(pitch_data[i]["pitch_type"])
+
+    ax.scatter(pitch_data[i]["px"], pitch_data[i]["pz"], label = label, c = color, marker = marker)
+
+print("{ball_ct}ball {st_ct}strike".format(ball_ct = pitch_data[pitch]["pa_ball_ct"], st_ct = pitch_data[pitch]["pa_strike_ct"]))
+print("{out_cn}out".format(out_cn = pitch_data[pitch]["event_outs_ct"] - 1))
+
+pitch += 1
+cnt +=1
+
+if len(pitch_data) < pitch:
+    pitch = 1
+    i = 1
+    for _, d in data.iterrows():
+        if i == cnt:
+            inning = d["inning_number"]
+            batter = d["bat_box_name"]
+            break
+        i += 1
+
+ax.legend()
+
